a849e2d1d9247cd8d3a7dfe52d46213f149c97a3
David Pestana Perdomo <david.pestana.perdomo@gmail.com>
2025-10-07 10:15:19 +0000

    ajuste de minimo viable en ejemplo

--- test/ejempo_test.py
+++ test/ejempo_test.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_argument("--headless=new")
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+# service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(options=options)
 
 driver.get("https://example.com")
 print(driver.title)
