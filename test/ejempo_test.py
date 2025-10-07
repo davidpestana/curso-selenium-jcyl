4c2214548e74ac6d7b0986fcc809df4962829980
David Pestana Perdomo <david.pestana.perdomo@gmail.com>
2025-10-07 09:42:09 +0000

    fixes minimo viable

--- test/ejempo_test.py
+++ test/ejempo_test.py
@@ -4,11 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
-options.binary_location = "/usr/bin/google-chrome"
 options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
